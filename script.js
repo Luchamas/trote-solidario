5fac1583824c7dd785b38477101bb79acabf8440
Lucas <109311445+Luchamas@users.noreply.github.com>
2023-03-27 22:18:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -38,3 +38,20 @@ function calcularSangue() {
   document.getElementById("calculo2").innerHTML =
     "Seu time pontuou " + resultado + " pontos"
 }
+
+function trocaSwitch() {
+  // vamos obter a página ou documento HTML
+  // na verdade, obteremos o elemento html (root)
+  const html = document.documentElement
+  // vamos verificar se no documento tem a classe claro
+  // if é o comando se
+  if (html.classList.contains("light")) {
+    // como o documento tem a classe claro, vamos remover
+    html.classList.remove("light")
+    html.classList.add("night")
+  } else {
+    // como o documento não tem a classe claro, vamos adicionar
+    html.classList.add("light")
+    html.classList.remove("night")
+  }
+}
